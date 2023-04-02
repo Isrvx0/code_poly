3ec90d5a5fe70caa302b8753afdff9f00a377465
Israa Almahmoud <99071791@mydavinci.nl>
2023-04-03 01:39:26 +0200

    easy questions

---
+++ questions.py
@@ -0,0 +1,81 @@
+possible_choices = ["1" , "2" , "3" , "4"]
+
+easy_questions = [
+    {
+        "question" : "Wat is een Variable? ",
+        "choices" : "1. Variabelen zijn containers die bedoeld zijn om dingen niet binnen te houden.\n2. Variabelen zijn containers voor het opslaan van gegevenswaarden.\n3.Een variabele in een python-programma geeft gegevens aan de computer voor verwerking.",
+        "answer" : "2"
+    },{
+        "question" : "Wat is een String?",
+        "choices" : "1. Strings is een nummer. \n2.Strings is een zin of een word. \n3.String is een decimaal getal  ",
+        "answer" : "2"
+    },{
+        "question" : "Wat doet een input? ",
+        "choices" : "1. Een bepaalde tekst in een tekst toevoegen. \n2.Een vraag stellen aan de gebruiker. \n3.Een vraag stellen aan de programmeur  ",
+        "answer" : "1"
+    },{
+        "question" : "Wat doet een elif? ",
+        "choices" : "1. Een voorwaarde om iets te checken. \n2. Een input om een vraag te stellen. \n3.Als de vorige voorwaarden niet waar waren, probeer dan deze. \n4.Geen van de bovenste.",
+        "answer" : "3"
+    },{
+        "question" : "Welke van deze is een float?",
+        "choices" : "1. 4 \n2. 5,443 \n3. 3.59 \n4. 20093",
+        "answer" : "3"
+    },{
+        "question" : "Welke operator geeft aan dat het getal a gelijk is aan getal b?",
+        "chocies" : "1. a = b \n2. a != b \n3. a += b \n4. a == b",
+        "answer" : "4"
+    },{
+        "question" : " Wat betekent a > b",
+        "chocies" : "1. a is klijner dan b \n2. a is groter dan b \n3. a is gelijk aan of klijner dan b  \n4. a is gelijk aan of groter dan b",
+        "answer" : "2"
+    },{
+        "question" : "Welke van de volgende is een dictionary:",
+        "chocies" : "1. { } \n2. [] \n3. () \n4. <>",
+        "answer" : "1"
+    },{
+        "question" : "Welke van de volgende is de juiste extensie van een Python-bestand?",
+        "chocies" : "1. .python \n2. .py \n3. .pl \n4. .p",
+        "answer" : "2"
+    },{
+        "question" : "Welk trefwoord wordt gebruikt voor functie in Python?",
+        "chocies" : "1. function \n2. fun \n3. define \n4. def",
+        "answer" : "4"
+    },{
+        "question" : "Welke van de volgende is niet een core data-type in Python?",
+        "chocies" : "1. list \n2. class \n3. tuple \n4. dictionary",
+        "answer" : "2"
+    },{
+        "question" : "Welke van de volgende is een tuple:",
+        "chocies" : "1. { } \n2. [] \n3. () \n4. <>",
+        "answer" : "3"
+    },{
+        "question" : "Wat is de output van \nprint(4 + 2 * 2)",
+        "chocies" : "1. 12 \n2. 8 \n3. 6 \n4. 4 + 2 * 2",
+        "answer" : "2"
+    },{
+        "question" : "Welke antwoord is niet een keyword?",
+        "chocies" : "1. both \n2. not \n3. or \n4. none ",
+        "answer" : "1"
+    },{
+        "question" : "Wat gebeurt er als je een 'break' in een while loop zet?",
+        "chocies" : "1. de while loop loopt nog 1 keer en stopt \n2. de while loop stopt \n3.de while loop gaat verder \n4. Niks :)",
+        "answer" : "2"
+    },{
+        "question" : " Wat doet een random.shuffle?",
+        "chocies" : "1. het geeft een random geselecteerd element uit een opgegeven reeks \n2. het reorganiseert de volgorde van de items.",
+        "answer" : "2"
+    },{
+        "question" : "Wat is de output van het volgende stukje code? \nabc = 'ABC zijn de eerste Drie letters van het alfabet.' \nprint(abc.lower())",
+        "chocies" : "1. Abc zijn de eerste drie letters van het alfabet. \n2. ABC zijn de eerste drie letters van het alfabet. \n3. abc zijn de eerste drie letters van het alfabet.\n4. ABC zijn de eerste Drie letters van het alfabet.",
+        "answer" : "3"
+    },{
+        "question" : "Wat doet een len?",
+        "chocies" : "1. geeft het aantal items in een object terug \n2. geeft het lengte van een object terug \n3.geeft het aantal letters in een object terug",
+        "answer" : "1"
+    },{
+        "question" : "Wanneer wordt het Final-blok uitgevoerd in een exception handling?",
+        "chocies" : "1. wanneer er geen uitzondering is \n2. altijd \n3. wanneer er een uitzondering is\n4. alleen als aan een bepaalde voorwaarde is voldaan",
+        "answer" : "2"
+    }
+]
